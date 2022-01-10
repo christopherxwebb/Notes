569244c812206a440dd87294f0a013d4b1d78924
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-10 15:51:57 +0000

    jest tests written for getNotes, addNote and reset

---
+++ tests/notesModel.test.js
@@ -0,0 +1,17 @@
+describe('class NotesModel', () => {
+  beforeEach(() => {
+    model = new NotesModel()
+  })
+  it('returns a blank array for getNotes initially', () => {
+    expect(model.getNotes()).toEqual([]);
+  })
+  it('adds a entry when you call addNote', () => {
+    model.addNote('Buy Milk');
+    expect(model.getNotes()).toEqual(['Buy Milk']);
+  })
+  it('empties the notes when you call reset', () => {
+    model.addNote('Buy Milk');
+    model.reset()
+    expect(model.getNotes()).toEqual([]);
+  })
+})
